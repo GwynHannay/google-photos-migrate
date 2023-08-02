af1ffe8093d175cc78e6f55d7fc89d495cb58a6a
Gwyn Hannay <gwyn@hannay.id.au>
2023-08-02 09:16:16 +0800

    script running

--- duplicates/find-duplicate-files.py
+++ duplicates/find-duplicate-files.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 import os
-import cv2
 import re
-import sqlite3
 import shutil
+import sqlite3
+
+import cv2
+import exiftool
+from datetime import datetime, timedelta
 from skimage.metrics import structural_similarity as ssim
 
 other_locations = [
     "/Volumes/array/storage/photos/Zenphoto Backup/albums/2004",
-    "/Volumes/array/storage/photos/Zenphoto Backup/albums/2005"
+    "/Volumes/array/storage/photos/Zenphoto Backup/albums/2005/2005-01"
 ]
 
 data_dir = "../output"
@@ -39,9 +42,6 @@ similarity_threshold = 0.8
 
 db = "duplicates.db"
 
-duplicates = "./duplicates_data.txt"
-open(duplicates, "w").close()
-
 
 def create_db():
     """Create SQLite database with tables for Google Photos and duplicates.
@@ -58,17 +58,21 @@ def create_db():
           distinct_name TEXT NOT NULL,
           original_name TEXT NOT NULL,
           filepath TEXT NOT NULL,
-          size INTEGER
+          size INTEGER,
+          created_date TEXT
         );"""
         cursor.execute(main_table_query)
         conn.commit()
 
         duplicates_table_query = """CREATE TABLE duplicate_photos (
           id INTEGER PRIMARY KEY,
-          original_filepath TEXT NOT NULL,
+          google_photo_id INTEGER NOT NULL,
           duplicate_filepath TEXT NOT NULL,
           similarity_score TEXT NOT NULL,
-          size INTEGER
+          size INTEGER,
+          created_date TEXT,
+          new_path TEXT,
+          FOREIGN KEY(google_photo_id) REFERENCES google_photos(id)
         );"""
         cursor.execute(duplicates_table_query)
         conn.commit()
@@ -84,8 +88,8 @@ def insert_original_records(records: list):
 
     Args:
         records (list): Google Photo information such as distinct filename (having 
-            removed rounded brackets), original filename, filepath on system and file 
-            size.
+            removed rounded brackets), original filename, filepath on system, file 
+            size and the extracted original datetime.
 
     Raises:
         Exception: General exception.
@@ -95,8 +99,8 @@ def insert_original_records(records: list):
         cursor = conn.cursor()
 
         insert_query = """INSERT INTO google_photos
-          (distinct_name, original_name, filepath, size)
-          VALUES (?, ?, ?, ?);"""
+          (distinct_name, original_name, filepath, size, created_date)
+          VALUES (?, ?, ?, ?, ?);"""
 
         cursor.executemany(insert_query, records)
         conn.commit()
@@ -113,9 +117,10 @@ def insert_duplicates_records(records: list):
     """Inserts multiple records of duplicate pictures found.
 
     Args:
-        records (list): Duplicate picture information such as the filepath of the 
-            original (matches google_photos.filepath), filepath of the duplicate,
-            similarity score from skimage.metrics.structural_similarity, and file size.
+        records (list): Duplicate picture information with the id of matching Google 
+            Photo, filepath of the duplicate, similarity score from 
+            skimage.metrics.structural_similarity, file size, and extracted original 
+            datetime.
 
     Raises:
         Exception: General exception.
@@ -125,8 +130,8 @@ def insert_duplicates_records(records: list):
         cursor = conn.cursor()
 
         insert_query = """INSERT INTO duplicate_photos
-          (original_filepath, duplicate_filepath, similarity_score, size)
-          VALUES (?, ?, ?, ?);"""
+          (google_photo_id, duplicate_filepath, similarity_score, size, created_date)
+          VALUES (?, ?, ?, ?, ?);"""
 
         cursor.executemany(insert_query, records)
         conn.commit()
@@ -154,7 +159,7 @@ def check_db(filename: str) -> list:
         conn = sqlite3.connect(db)
         cursor = conn.cursor()
 
-        filename_query = """SELECT filepath FROM google_photos 
+        filename_query = """SELECT id, filepath FROM google_photos 
             WHERE distinct_name = ?"""
         cursor.execute(filename_query, (filename,))
         matches = cursor.fetchall()
@@ -182,9 +187,9 @@ def find_best_copies() -> list:
         conn = sqlite3.connect(db)
         cursor = conn.cursor()
 
-        filename_query = """SELECT dp.original_filepath, dp.duplicate_filepath
+        filename_query = """SELECT dp.id, dp.duplicate_filepath
             FROM google_photos gp
-            JOIN duplicate_photos dp ON dp.original_filepath = gp.filepath
+            JOIN duplicate_photos dp ON dp.google_photo_id = gp.id
             WHERE dp.size > gp.size;"""
         cursor.execute(filename_query)
         matches = cursor.fetchall()
@@ -197,7 +202,61 @@ def find_best_copies() -> list:
         raise Exception(f"Could not query database: {e}")
 
 
-def copy_file(filepath: str, duplicate_index: int=0) -> str:
+def add_new_locations(records: list):
+    """Updates duplicate photos table with new filepaths.
+
+    Args:
+        records (list): ID of the record to update, and the new filepath to add.
+
+    Raises:
+        Exception: General exception.
+    """
+    try:
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+
+        update_query = """UPDATE duplicate_photos
+          SET new_path = ?
+          WHERE id = ?"""
+
+        cursor.executemany(update_query, records)
+        conn.commit()
+        print("Total", cursor.rowcount, "records updated")
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        raise Exception(f"Could not update records: {e}")
+
+
+def get_most_accurate_dates():
+    try:
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+
+        filename_query = """SELECT 
+                dp.new_path,
+                gp.created_date AS google_date,
+                dp.created_date as duplicate_date,
+                gp.filepath
+            FROM google_photos gp
+            JOIN duplicate_photos dp ON dp.google_photo_id = gp.id
+            WHERE dp.new_path IS NOT NULL AND 
+                (dp.created_date > gp.created_date
+                OR dp.created_date = 'None');"""
+        cursor.execute(filename_query)
+        matches = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return matches
+    except Exception as e:
+        raise Exception(f"Could not query database: {e}")
+
+
+def copy_file(filepath: str, duplicate_index: int = 0) -> str:
     basename = os.path.basename(filepath)
 
     if duplicate_index > 0:
@@ -246,53 +305,94 @@ def process_file(file: str) -> list:
     # Check if a corresponding file exists in the database
     matches = check_db(basename)
 
-    for match in matches:
-        data_file = match
-        if os.path.exists(data_file):
-            # Load and compare videos
-            storage_video = cv2.VideoCapture(file)
-            data_video = cv2.VideoCapture(data_file)
+    with exiftool.ExifToolHelper() as et:
+        for match in matches:
+            data_file = match[1]
+            if os.path.exists(data_file):
+                # Load and compare videos
+                storage_video = cv2.VideoCapture(file)
+                data_video = cv2.VideoCapture(data_file)
 
-            similarity_sum = 0
-            frame_count = 0
+                similarity_sum = 0
+                frame_count = 0
 
-            while True:
-                # Read frames from the videos
-                ret1, storage_frame = storage_video.read()
-                ret2, data_frame = data_video.read()
+                while True:
+                    # Read frames from the videos
+                    ret1, storage_frame = storage_video.read()
+                    ret2, data_frame = data_video.read()
 
-                if not ret1 or not ret2:
-                    break
+                    if not ret1 or not ret2:
+                        break
 
-                similarity = compare_frames(storage_frame, data_frame)
-                similarity_sum += similarity
-                frame_count += 1
+                    similarity = compare_frames(storage_frame, data_frame)
+                    similarity_sum += similarity
+                    frame_count += 1
 
-            average_similarity = similarity_sum / frame_count if frame_count > 0 else 0
+                average_similarity = similarity_sum / frame_count if frame_count > 0 else 0
 
-            if average_similarity >= similarity_threshold:
-                file_size = os.stat(file).st_size
-                return_records.append(
-                    (data_file, file, str(average_similarity), file_size))
+                if average_similarity >= similarity_threshold:
+                    google_photo_id = match[0]
+                    file_size = os.stat(file).st_size
+                    created_date = et.get_tags(file, "DateTimeOriginal")
 
-            storage_video.release()
-            data_video.release()
-    return return_records
+                    if created_date[0].get('XMP:DateTimeOriginal'):
+                        original_datetime = created_date[0].get(
+                            'XMP:DateTimeOriginal')
+                    elif created_date[0].get('EXIF:DateTimeOriginal'):
+                        original_datetime = created_date[0].get(
+                            'EXIF:DateTimeOriginal')
+                    elif created_date[0].get('QuickTime:DateTimeOriginal'):
+                        original_datetime = created_date[0].get(
+                            'QuickTime:DateTimeOriginal')
+                    else:
+                        original_datetime = 'None'
+
+                    return_records.append(
+                        (google_photo_id,
+                         file,
+                         str(average_similarity),
+                         file_size,
+                         original_datetime)
+                    )
+
+                storage_video.release()
+                data_video.release()
+        return return_records
 
 
 def index_photos():
     db_records = []
-    for root, dirs, files in os.walk(data_dir):
-        for file in files:
-            if any(file.lower().endswith(ext) for ext in extensions):
-                cleaned_filename = re.sub(r"\s\([^)]*\)\.", ".", file)
-                file_path = os.path.join(root, file)
-                file_size = os.stat(file_path).st_size
-                db_records.append(
-                    (cleaned_filename, file, file_path, file_size))
-        insert_original_records(db_records)
-        db_records = None
-        db_records = []
+    with exiftool.ExifToolHelper() as et:
+        for root, dirs, files in os.walk(data_dir):
+            for file in files:
+                if any(file.lower().endswith(ext) for ext in extensions):
+                    cleaned_filename = re.sub(r"\s\([^)]*\)\.", ".", file)
+                    file_path = os.path.join(root, file)
+                    file_size = os.stat(file_path).st_size
+                    created_date = et.get_tags(file_path, "DateTimeOriginal")
+
+                    if created_date[0].get('XMP:DateTimeOriginal'):
+                        original_datetime = created_date[0].get(
+                            'XMP:DateTimeOriginal')
+                    elif created_date[0].get('EXIF:DateTimeOriginal'):
+                        original_datetime = created_date[0].get(
+                            'EXIF:DateTimeOriginal')
+                    elif created_date[0].get('QuickTime:DateTimeOriginal'):
+                        original_datetime = created_date[0].get(
+                            'QuickTime:DateTimeOriginal')
+                    else:
+                        original_datetime = 'None'
+
+                    db_records.append(
+                        (cleaned_filename,
+                         file,
+                         file_path,
+                         file_size,
+                         original_datetime)
+                    )
+            insert_original_records(db_records)
+            db_records = None
+            db_records = []
 
 
 def find_duplicates(storage_dir: str):
@@ -312,15 +412,72 @@ def find_duplicates(storage_dir: str):
 
 
 def fetch_best_copies():
+    new_copies = []
     better_copies = find_best_copies()
     if better_copies:
         if not os.path.exists(local_dir):
             os.mkdir(local_dir)
 
     for best_copy in better_copies:
-        new_location = copy_file(best_copy[1])
-        with open(duplicates, "a") as f:
-            f.write(f"{new_location} - {best_copy[0]}\n")
+        duplicate_photo_id = best_copy[0]
+        duplicate_filepath = best_copy[1]
+
+        new_location = copy_file(duplicate_filepath)
+        new_copies.append((new_location, duplicate_photo_id))
+
+    add_new_locations(new_copies)
+
+
+def replace_photos():
+    new_files = get_most_accurate_dates()
+
+    with exiftool.ExifTool() as et:
+        for duplicate in new_files:
+            timezone_safety_date = None
+            change_date = False
+            print(duplicate)
+
+            duplicate_file = duplicate[0]
+            google_date_string = duplicate[1]
+            duplicate_date_string = duplicate[2]
+            google_file = duplicate[3]
+
+            google_date = format_date_string(google_date_string)
+            duplicate_date = format_date_string(duplicate_date_string)
+
+            if isinstance(google_date, datetime):
+                if isinstance(duplicate_date, datetime):
+                    timezone_safety_date = google_date + timedelta(hours=8)
+
+                    if duplicate_date > timezone_safety_date:
+                        change_date = True
+                else:
+                    change_date = True
+
+                if change_date:
+                    et.execute("-TagsFromFile", google_file, "-alldates",
+                               "-FileModifyDate", duplicate_file, "-overwrite_original")
+
+            shutil.move(duplicate_file, google_file)
+
+
+def format_date_string(datestring: str) -> datetime | None:
+    new_date = None
+
+    match len(datestring):
+        case 19:
+            new_date = datetime.strptime(
+                datestring, '%Y:%m:%d %H:%M:%S')
+        case 28:
+            new_date = datetime.strptime(
+                datestring, '%Y:%m:%d %H:%M:%S.%f%z')
+        case 24:
+            new_date = datetime.strptime(
+                datestring, '%Y:%m:%d %H:%M:%S.%fZ')
+        case _:
+            pass
+
+    return new_date
 
 
 if __name__ == "__main__":
@@ -333,11 +490,14 @@ if __name__ == "__main__":
         print("Storing records of Google Photos in database")
         index_photos()
 
-        # Check for duplicates from other locations
-        for location in other_locations:
-            print(f"Searching location for duplicates: {location}")
-            find_duplicates(location)
+    # Check for duplicates from other locations
+    for location in other_locations:
+        print(f"Searching location for duplicates: {location}")
+        find_duplicates(location)
 
     # Get the best copies to local folder
     print("Getting higher quality pictures")
     fetch_best_copies()
+
+    # Fix any incorrect dates on duplicate images, then replace Google Photos
+    replace_photos()
